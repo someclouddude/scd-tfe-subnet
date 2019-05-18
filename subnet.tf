14db2c259bc3ec1d499a8c7452f27eeb64261509
Ian Evans <someclouddude@gmail.com>
2019-05-18 16:20:46 -0500

    adding remote state key

--- subnet.tf
+++ subnet.tf
@@ -11,6 +11,6 @@ data "terraforom_remote_state" "vpc" {
 }
 
 resource "aws_subnet" "test" {
-  vpc_id = "${data.terraform_remote_state.vpc}"
+  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
   cidr_block = "10.51.3.0/25"
 }
